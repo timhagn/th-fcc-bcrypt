b7fd2c55b8e55a34bbbb7c12a21324c7222a9d50
timhagn <mail@timhagn.com>
2018-10-03 19:13:39 +0200

    add bcrypt

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 const app         = express();
+const bcrypt      = require('bcrypt');
 
 fccTesting(app); //For FCC testing purposes
 
@@ -12,7 +13,12 @@ const someOtherPlaintextPassword = 'pass123';
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+  console.log(hash);
+  bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+    console.log(res);
+  });
+});
 
 
 //END_ASYNC
